8ac763407507363867ce584f780592bd335d4977
明 <akariondev@gmail.com>
2023-11-24 15:58:14 -0300

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -1,10 +1,8 @@
-### AnyKernel3 Ramdisk Mod Script
-## osm0sis @ xda-developers
+### MoeKernel - Xiaomi Redmi Note 8
+## Akari Azusagawa @ github-dev
 
-### AnyKernel setup
-# begin properties
 properties() { '
-kernel.string=Moe by whyakari
+kernel.string=MoeKernel by whyakari
 do.devicecheck=1
 do.modules=0
 do.systemless=1
@@ -14,25 +12,19 @@ device.name1=ginkgo
 device.name2=willow
 supported.versions=12.0-14.0
 supported.patchlevels=
-'; } # end properties
+'; }
 
-### AnyKernel install
-# begin attributes
 attributes() {
 set_perm_recursive 0 0 755 644 $ramdisk/*;
 set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
-} # end attributes
+}
 
-## boot shell variables
 block=/dev/block/bootdevice/by-name/boot;
 is_slot_device=0;
 ramdisk_compression=auto;
 patch_vbmeta_flag=auto;
 
-# import functions/variables and setup patching - see for reference (DO NOT REMOVE)
 . tools/ak3-core.sh && attributes;
 
-# boot install
-dump_boot; # use split_boot to skip ramdisk unpack, e.g. for devices with init_boot ramdisk
-write_boot; # use flash_boot to skip ramdisk repack, e.g. for devices with init_boot ramdisk
-## end boot install
+dump_boot;
+write_boot;
